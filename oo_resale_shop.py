82f07570872302447cf4f3314614b6286f36472e
Emily Middleton <emily.a.middleton@gmail.com>
2024-09-14 08:07:25 -0400

    Added a sell method, but I have a new issue with printing inventory where it adds : None at the end. It's not crashing or anything though

--- oo_resale_shop.py
+++ oo_resale_shop.py
@@ -4,28 +4,36 @@ from typing import Dict
 
 class ResaleShop():
 
-    # What attributes will it need?
+    # Attributes
     inventory : Dict[int, Computer] = {}
     itemID: int
-    # How will you set up your constructor?
-    # Remember: in python, all constructors have the same name (__init__)
+   
+    #Constructor
     def __init__(self):
         self.inventory:dict = {}
         self.itemID = 0
 
+    #PC is the name of my variable, of type Computer, so I can pass in the instance of the computer
     def buy(self, PC:Computer):
         self.itemID += 1 # increment itemID
         self.inventory[self.itemID] = PC
 
-##when I want to add computer info into parameters, I have to say like, def name(self, myComputer: Computer)
-    # What methods will you need?
+    #okay, current issue is arguments? Do I pass in the computer? I think I have to.
+    #but then I don't use PC at all?
+    def sell(self, PC:Computer):
+        if self.itemID in self.inventory:
+            del self.inventory[self.itemID]
+            print("Item", self.itemID, "sold!")
+        else: 
+            print("Item", self.itemID, "not found. Please select another item to sell.")
 
-    #am trying to print the inventory, it doesn't quite work though
+    #prints out the inventory if it exists
+    #Confused because now it's printing out : None at the end? Why?
     def output(self):
         if self.inventory:
         # For each item
             for self.itemID in self.inventory:
-            # Print its details
+            # Print its details, using vomit() method from computer class
                 print(f'Item ID: {self.itemID} : {self.inventory[self.itemID].vomit()}')
         else:
             print("No inventory to display.")
@@ -40,6 +48,13 @@ def main():
 
     myShop = ResaleShop()
     myShop.buy(my_computer)
+    myShop.output()
+
+    print()
+    print()
+    print()
+
+    myShop.sell(my_computer)
     myShop.output()
 
 main()
