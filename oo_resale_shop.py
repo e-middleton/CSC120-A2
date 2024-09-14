db620a2fa9c7d4b62f7a9ce4ef73ff699607c34d
Emily Middleton <emily.a.middleton@gmail.com>
2024-09-14 07:45:07 -0400

    Added attributes and constructor, maybe added the buy method? Trying to debug printing inventory

--- oo_resale_shop.py
+++ oo_resale_shop.py
@@ -1,10 +1,45 @@
-class ResaleShop:
+from computer import Computer
+from typing import Dict
+
+
+class ResaleShop():
 
     # What attributes will it need?
-
+    inventory : Dict[int, Computer] = {}
+    itemID: int
     # How will you set up your constructor?
     # Remember: in python, all constructors have the same name (__init__)
-    def __init__():
-        pass # You'll remove this when you fill out your constructor
+    def __init__(self):
+        self.inventory:dict = {}
+        self.itemID = 0
 
+    def buy(self, PC:Computer):
+        self.itemID += 1 # increment itemID
+        self.inventory[self.itemID] = PC
+
+##when I want to add computer info into parameters, I have to say like, def name(self, myComputer: Computer)
     # What methods will you need?
+
+    #am trying to print the inventory, it doesn't quite work though
+    def output(self):
+       if self.inventory:
+        # For each item
+        for self.itemID in self.inventory:
+            # Print its details
+            print(f'Item ID: {self.itemID} : {self.inventory[self.itemID]}')
+        else:
+            print("No inventory to display.")
+
+def main():
+    my_computer = Computer(
+        "Mac Pro (Late 2013)",
+        "3.5 GHc 6-Core Intel Xeon E5",
+        1024, 64,
+        "macOS Big Sur", 2013, 1500
+    )
+
+    myShop = ResaleShop()
+    myShop.buy(my_computer)
+    myShop.output()
+
+main()
